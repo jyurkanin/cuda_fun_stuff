ed8b8d44ebe1821970f37f845a0162e75028130a
Justin Yurkanin <yurkanin321@gmail.com>
2022-04-19 14:36:30 -0500

    whatever

--- src/cool.cu
+++ src/cool.cu
@@ -8,26 +8,83 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 640;
 
-//abs((int)x[i]-960)*abs((int)y[i]-540)/param;
+__device__
+void HSVtoRGB(float& fR, float& fG, float& fB, float& fH, float& fS, float& fV) {
+  float fC = fV * fS; // Chroma
+  float fHPrime = fmod(fH / 60.0f, 6.0f);
+  float fX = fC * (1 - fabs(fmod(fHPrime, 2.0f) - 1));
+  float fM = fV - fC;
+  
+  if(0 <= fHPrime && fHPrime < 1) {
+    fR = fC;
+    fG = fX;
+    fB = 0;
+  } else if(1 <= fHPrime && fHPrime < 2) {
+    fR = fX;
+    fG = fC;
+    fB = 0;
+  } else if(2 <= fHPrime && fHPrime < 3) {
+    fR = 0;
+    fG = fC;
+    fB = fX;
+  } else if(3 <= fHPrime && fHPrime < 4) {
+    fR = 0;
+    fG = fX;
+    fB = fC;
+  } else if(4 <= fHPrime && fHPrime < 5) {
+    fR = fX;
+    fG = 0;
+    fB = fC;
+  } else if(5 <= fHPrime && fHPrime < 6) {
+    fR = fC;
+    fG = 0;
+    fB = fX;
+  } else {
+    fR = 0;
+    fG = 0;
+    fB = 0;
+  }
+  
+  fR += fM;
+  fG += fM;
+  fB += fM;
+}
 
 __global__
-void compute_pixel(int n, int param, float *x, float *y, unsigned int *pixel){
+void compute_pixel(int n, float param, float *x, float *y, unsigned int *pixel, unsigned int *prev_pixel){
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
     int stride = blockDim.x * gridDim.x;
     
+    float red;
+    float green;
+    float blue;
+    
+    unsigned red_u;
+    unsigned green_u;
+    unsigned blue_u;
+    
+    float hue;
+    float sat = 1;
+    float val = 1;
+    
     for(int i = idx; i < n; i += stride){
-        float z1 = x[i];
-        float z2 = y[i];
-        float dz1;
-        float dz2;
-        for(int j = 0; j < 1000; j++){
-            dz1 = z1 + z2;
-            dz2 = 1.0f/(param*z1*z2) - (z2*z1)/((float)param);
-            z1 += dz1*.01;
-            z2 += dz2*.01;
+        float r1 = x[i];
+        float r2 = y[i];
+        float nr1;
+        int j;
+        for(j = 0; j < 1000 && ((r1*r1)+(r2*r2)) < 36; j++){
+            r1 =  r1 + param/r2; //looks like a sunset around param <= -7
+            r2 = -r2 + 1/r1 + 1/r2;
+            r1 = nr1;
         }
+
+        //hue = 350.0f*j/1000;
+        red_u = (unsigned) 85*log10f(1 + (float)j);
+        green_u = 0;
+        blue_u = 0;
+        pixel[i] = (red_u << 16) + (green_u << 8) + blue_u;
         
-        pixel[i] = (int)0xFF*(z1+z2);
+        
     }
 }
 
@@ -81,10 +138,12 @@ int main( int argc, char* args[]){
     printf("My device is #%d\n", device);
     
     unsigned int *cuda_pixel_buf = 0; //stores result.
+    unsigned int *cuda_pixel_buf_prev = 0;
     float *cuda_x_buf;
     float *cuda_y_buf;
     
     cudaMallocManaged(&cuda_pixel_buf, num_pixels*sizeof(unsigned int));
+    cudaMallocManaged(&cuda_pixel_buf_prev, num_pixels*sizeof(unsigned int));
     cudaMallocManaged(&cuda_x_buf, num_pixels*sizeof(float));
     cudaMallocManaged(&cuda_y_buf, num_pixels*sizeof(float));
     
@@ -93,13 +152,14 @@ int main( int argc, char* args[]){
     unsigned int cnt = 0;
     for(int y = 0; y < height; y++){
         for(int x = 0; x < width; x++){
-            cuda_x_buf[cnt] = (x/(float)width) - (1/2.0f);
-            cuda_y_buf[cnt] = (y/(float)height) - (1/2.0f);
+            cuda_x_buf[cnt] = (10*x/(float)width) - 5.0f;
+            cuda_y_buf[cnt] = (10*y/(float)height) - 5.0f;
+            cuda_pixel_buf_prev[cnt] = 0;
             cnt++;
         }
     }
     
-    int param = 0;
+    float param = 1;
     while(1){
         SDL_Event event;
         while(SDL_PollEvent(&event)) {
@@ -124,14 +184,18 @@ int main( int argc, char* args[]){
         
         int blockSize = 256;
         int numBlocks = (int) ceilf(num_pixels / blockSize);
-        printf("param %d\n", param);
-        compute_pixel<<<numBlocks, blockSize>>>(num_pixels, param, cuda_x_buf, cuda_y_buf, cuda_pixel_buf);
+        printf("param %f\n", param);
+        compute_pixel<<<numBlocks, blockSize>>>(num_pixels, param, cuda_x_buf, cuda_y_buf, cuda_pixel_buf, cuda_pixel_buf_prev);
         cudaDeviceSynchronize();
-        
-        for(int ii = 0; ii < num_pixels; ii++){
+
+        pixels[0] = cuda_pixel_buf[0];
+        cuda_pixel_buf_prev[0] = cuda_pixel_buf[num_pixels-1];
+        for(int ii = 1; ii < num_pixels; ii++){
             pixels[ii] = cuda_pixel_buf[ii];
+            cuda_pixel_buf_prev[ii] = cuda_pixel_buf[ii-1];
+            
         }
-        param++;
+        param -= .01f;
         SDL_UpdateWindowSurface(window);
     }
     
